8cb82ff145b094b1d7648da717825fd008fd4d05
BlancoSebastianEzequiel <sebastian.e.blanco@gmail.com>
2017-09-30 21:45:23 -0300

    Se agrego dos comandos que van por terminal (--ibuf-bytes, --obytes.bytes, -I, -O) pero no se probo si funciona

--- tp1.c
+++ tp1.c
@@ -1,15 +1,14 @@
 #define _POSIX_C_SOURCE 1
 #include <stdio.h>
 #include <string.h>
-#include <ctype.h>
 #include <stdbool.h>
 #include <stdlib.h>
 //------------------------------------------------------------------------------
 // DEFINITIONS
 //------------------------------------------------------------------------------
-#define ERROR 1
+#define ERROR -1
 #define SUCCESS 0
-#define VERSION "0.2"
+#define VERSION "0.1"
 const char help_str[] = "Usage:\n"
         "  tp1 -h\n"
         "  tp1 -V\n"
@@ -19,8 +18,10 @@ const char help_str[] = "Usage:\n"
         "  -h, --help\tPrint this information.\n"
         "  -i, --input\tLocation of the input file.\n"
         "  -o, --output\tLocation of the output file.\n"
+        "  -I, --ibuf-bytes\tbyte-count of the input buffer"
+        "  -O, --obuf-bytes\tbyte-count of the output buffer"
         "Examples:\n"
-        "  tp0 -i ~/input -o\n";
+        "  tp0 -i ~/input -o ~/output\n";
 //------------------------------------------------------------------------------
 // EXTERNAL FUNCTIONS
 //------------------------------------------------------------------------------
@@ -34,11 +35,12 @@ bool equal(const char* str1, const char* str2) {
 //------------------------------------------------------------------------------
 // ARG PARSE
 //------------------------------------------------------------------------------
-int argParse(int argc, char** argv, FILE** descriptors, int* clean_exit) {
+int argParse(int argc, char** argv, FILE** descriptors, size_t** ref) {
     int arg = 1;
     const int size = 8;
     const char* flags[] = {"-i", "-o", "-V", "-h", "--version", "--help",
-                           "--input", "--output"};
+                           "--input", "--output", "-I", "-O", "--ibuf-bytes",
+                           "--obuf-bytes"};
 
     bool std;
     char* flag = "";
@@ -56,12 +58,12 @@ int argParse(int argc, char** argv, FILE** descriptors, int* clean_exit) {
 
             if (equal(flag, "-h") || equal(flag, "--help")) {
                 printf("%s\n", help_str);
-                *clean_exit = 1;
+                *ref[2] = 1;
                 return SUCCESS;
             }
             if (equal(flag, "-V") || equal(flag, "--version")) {
                 printf("tp0: version %s\n", VERSION);
-                *clean_exit = 1;
+                *ref[2] = 1;
                 return SUCCESS;
             }
             if (isFlagNull) {
@@ -78,6 +80,12 @@ int argParse(int argc, char** argv, FILE** descriptors, int* clean_exit) {
                 descriptors[1] = fopen(argv[arg], "w");
                 if (descriptors[1] == NULL) return ERROR;
             }
+            if ((equal(flag, "-I") || equal(flag, "--ibuf-bytes")) && !std) {
+                *ref[0] = (size_t)atoi(argv[arg]);
+            }
+            if ((equal(flag, "-O") || equal(flag, "--obuf-bytes")) && !std) {
+                *ref[1] = (size_t)atoi(argv[arg]);
+            }
             flag = "nullStr";
         }
         arg++;
@@ -89,8 +97,11 @@ int argParse(int argc, char** argv, FILE** descriptors, int* clean_exit) {
 //------------------------------------------------------------------------------
 int main(int argc, char** argv) {
     FILE* fdescriptors[2] = {stdin, stdout};
-    int clean_exit = 0;
-    if (argParse(argc, argv, fdescriptors, &clean_exit) == ERROR) return 1;
+    size_t ibytes = 1, obytes = 1;
+    size_t clean_exit = 0;
+    size_t* ref[3] = {&ibytes, &obytes, &clean_exit};
+    int s = argParse(argc, argv, fdescriptors, ref);
+    if (s == ERROR) return 1;
     if (clean_exit) return 0;  // finalizacion limpia, cuando se usa -h o -V
 
     FILE* archIn = fdescriptors[0];
@@ -99,10 +110,12 @@ int main(int argc, char** argv) {
     int fdOut = fileno(archOut);
     if (fdIn == -1 || fdOut == -1) return 1;
 
-    size_t ibytes = 40, obytes = 40;
-    if (palindrome(fdIn, ibytes, fdOut, obytes) < 0) return 1;
-    if (archOut != stdout && fclose(archOut) == EOF) return 1;
+    if (palindrome(fdIn, ibytes, fdOut, obytes) == -1) {
+        printf("palindrome returned -1\n");
+        return 1;
+    }
     if (archIn != stdin && fclose(archOut) == EOF) return 1;
+    if (archOut != stdout && fclose(archOut) == EOF) return 1;
     return 0;
 }
 //------------------------------------------------------------------------------
