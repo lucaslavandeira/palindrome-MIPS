f09268f9e27a3123b91d7d683573ee6b8c4f1cbc
BlancoSebastianEzequiel <sebastian.e.blanco@gmail.com>
2017-10-01 00:49:07 -0300

    Actualizacion del test

--- putchTest.c
+++ putchTest.c
@@ -27,7 +27,17 @@ int putch2(int fd, char* buffer, size_t oBytes, size_t* pos, char c) {
 // MAIN
 //------------------------------------------------------------------------------
 int main(int argc, char** argv) {
+    printf("Running putch test........... ");
     FILE* archIn = fopen("in.txt", "r");
+    if (archIn == NULL) {
+        printf("ERROR: fopen returned NULL\n");
+        return 1;
+    }
+    int s = fseek(archIn, 0, SEEK_SET);
+    if (s < 0) {
+        printf("ERROR: fseek returned %d\n", s);
+        return 1;
+    }
     FILE* archOut = stdout;
     char buffer[20];
     int fd = fileno(archOut);
@@ -35,20 +45,24 @@ int main(int argc, char** argv) {
     int c = getc(archIn);
     while (c != EOF) {
         if (putch(fd, buffer, 20, &pos, (char) c) == -1) {
-            printf("*************putch returned -1......ERROR*************\n");
+            printf("ERROR: putch returned -1\n");
             return 1;
         }
         c = getc(archIn);
     }
     if (putch(fd, buffer, 20, &pos, '\n') == -1) {
-        printf("*************putch returned -1......ERROR*************\n");
+        printf("ERROR: putch returned -1\n");
         return 1;
     }
-    if (putch(fd, buffer, 20, &pos, c) == -1) {
-        printf("*************putch returned -1......ERROR*************\n");
+    if (putch(fd, buffer, 20, &pos, (char)c) == -1) {
+        printf("ERROR: putch returned -1\n");
         return 1;
     }
-    fclose(archIn);
+
+    if (fclose(archIn) == EOF) {
+        printf("ERROR: fclose returned EOF\n");
+        return 1;
+    }
     return 0;
 }
 //------------------------------------------------------------------------------
